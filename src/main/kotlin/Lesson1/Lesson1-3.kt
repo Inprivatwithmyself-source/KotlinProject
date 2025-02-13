16a3d9539889295bf9c679279e082b1809b190c8
Bagdan Musienko <bogdanmusienko420@gmail.com>
2025-02-13 15:04:09 +0300

    KP-1-3 output time of first fly to space

---
+++ src/main/kotlin/Lesson1/Lesson1-3.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+    println(year)
+    println(hour)
+    println(minute)
+    hour = 10
+    minute = 55
+    print(hour)
+    print(":")
+    println(minute)
+    println("$hour:$minute") // Ну или так если не идти точно по задаче
+}
+

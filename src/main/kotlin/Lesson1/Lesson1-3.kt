15e98f021f5566f6a7058f9965480c51e5db1fe1
Bagdan Musienko <bogdanmusienko420@gmail.com>
2025-02-14 16:43:07 +0300

    KP-1-3 made and input variables

--- src/main/kotlin/Lesson1/Lesson1-3.kt
+++ src/main/kotlin/Lesson1/Lesson1-3.kt
@@ -2,14 +2,15 @@ fun main() {
     val year: Int = 1961
     var hour: Int = 9
     var minute: Int = 7
+
     println(year)
     println(hour)
     println(minute)
+
     hour = 10
     minute = 55
-    print(hour)
-    print(":")
-    println(minute)
-    println("$hour:$minute") // Ну или так если не идти точно по задаче
+
+    val timeOfSit = "$hour:$minute"
+    println(timeOfSit)
 }
 

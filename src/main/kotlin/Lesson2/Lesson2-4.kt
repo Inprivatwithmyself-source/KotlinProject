ba66127327555ed54a4ebdb38fcfa987a841acfd
Bagdan Musienko <bogdanmusienko420@gmail.com>
2025-02-17 20:49:07 +0300

    KS-2-4 work with buffs

--- src/main/kotlin/Lesson2/Lesson2-4.kt
+++ src/main/kotlin/Lesson2/Lesson2-4.kt
@@ -4,11 +4,11 @@ fun main() {
     val quantOfCrystal = 7
     val quantOfIron = 11
 
-    val crystalWithBuff = (quantOfCrystal * (1 + Buff/100)).toInt()
-    val ironWithBuff = (quantOfIron * (1 + Buff/100)).toInt()
+    val crystalWithBuff = (quantOfCrystal * (1 + BUFF/100)).toInt()
+    val ironWithBuff = (quantOfIron * (1 + BUFF/100)).toInt()
 
     println(crystalWithBuff)
     println(ironWithBuff)
 
 }
-const val Buff = 20.0
+const val BUFF = 20.0

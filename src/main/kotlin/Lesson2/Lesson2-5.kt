75966f1eab72edc6ab35b389e2ddae608e92b4b6
Bagdan Musienko <bogdanmusienko420@gmail.com>
2025-02-17 11:10:52 +0300

    KS-2-5 compound percent

--- src/main/kotlin/Lesson2/Lesson2-5.kt
+++ src/main/kotlin/Lesson2/Lesson2-5.kt
@@ -6,8 +6,8 @@ fun main() {
     val contribution = 70000
     val yearsOfContribution = 20.0
 
-    val compoundPercent = contribution * ((1 + percent).pow(yearsOfContribution))
+    val compoundPercent = contribution * ((1 + percent/100).pow(yearsOfContribution))
 
     println(String.format("%.3f", compoundPercent))
 }
-const val percent = 0.167
+const val percent = 16.7

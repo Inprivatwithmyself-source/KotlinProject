ac946220ac00fdbc17f04d4c071448a9dc2847b7
Bagdan Musienko <bogdanmusienko420@gmail.com>
2025-02-17 11:08:22 +0300

    KS-2.4 Buff

--- src/main/kotlin/Lesson2/Lesson2-4.kt
+++ src/main/kotlin/Lesson2/Lesson2-4.kt
@@ -4,11 +4,11 @@ fun main() {
     val quantOfCrystal = 7
     val quantOfIron = 11
 
-    val crystalWithBuff = (quantOfCrystal * (1 + Buff_In_Percent/100)).toInt()
-    val ironWithBuff = (quantOfIron * (1 + Buff_In_Percent/100)).toInt()
+    val crystalWithBuff = (quantOfCrystal * (1 + Buff/100)).toInt()
+    val ironWithBuff = (quantOfIron * (1 + Buff/100)).toInt()
 
     println(crystalWithBuff)
     println(ironWithBuff)
 
 }
-const val Buff_In_Percent = 20.0
+const val Buff = 20.0

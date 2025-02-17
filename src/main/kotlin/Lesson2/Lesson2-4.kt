1996ac0ed36c4717d695c472297c989522a2f488
Bagdan Musienko <bogdanmusienko420@gmail.com>
2025-02-17 11:06:42 +0300

    KS-3-3 table of multiple

--- src/main/kotlin/Lesson2/Lesson2-4.kt
+++ src/main/kotlin/Lesson2/Lesson2-4.kt
@@ -4,11 +4,11 @@ fun main() {
     val quantOfCrystal = 7
     val quantOfIron = 11
 
-    val crystalWithBuff = (quantOfCrystal * buffInPercent).toInt()
-    val ironWithBuff = (quantOfIron * buffInPercent).toInt()
+    val crystalWithBuff = (quantOfCrystal * (1 + Buff_In_Percent/100)).toInt()
+    val ironWithBuff = (quantOfIron * (1 + Buff_In_Percent/100)).toInt()
 
     println(crystalWithBuff)
     println(ironWithBuff)
 
 }
-const val buffInPercent = 1.2
+const val Buff_In_Percent = 20.0
